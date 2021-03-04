3992bcbbb711b26e742a87ca4e9c01c8bfeb1b97
anthonydominguez86 <adominguez11186@gmail.com>
2021-03-04 16:49:38 -0500

    added a failsafe URL directory

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,9 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "/../public/assets/notes.html"));
     });
 
-    
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/assets/index.html"));
+      });
 
 
 }
